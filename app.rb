5f5af40aeae1e3a2a9e41101167b553b4c55d284
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-21 08:33:11 -0700

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ class App < Sinatra::Base
     erb :food_form
   end
 
+  post '/food' do
+    # params.to_s #<-- if this were return, page would just show hash w/ :name, :favorite_food
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
   # Add your post route and action below
 
 end
